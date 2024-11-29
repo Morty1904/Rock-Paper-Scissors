980b7f67330c21924bb121b9898746b34f38faad
Morty1904 <oscarendercraft@gmail.com>
2024-11-29 12:38:02 +0100

    Added Testing

--- demo/src/test/java/com/example/SimpleRPSTest.java
+++ demo/src/test/java/com/example/SimpleRPSTest.java
@@ -1,106 +1,82 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
-
-import java.util.Random;
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class SimpleRPSTest {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.InputStream;
+import java.util.Random;
+
+public class SimpleRPSTest {
 
     @Test
-    void testRockBeatsScissors() {
-        Move rock = new Rock();
-        Move scissors = new Scissors();
-        assertTrue(rock.beats(scissors), "Rock should beat Scissors");
+    void testMainRockVsScissors() {
+        String simulatedInput = "1\n";
+        InputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        Random random = new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 2;
+            }
+        };
+        SimpleRPS.main(new String[0]);
+        String output = outputStream.toString();
+        assertTrue(output.contains("You: Rock"));
+        assertTrue(output.contains("Machine: Scissors"));
+        assertTrue(output.contains("You win!"));
+        System.setIn(System.in);
+        System.setOut(System.out);
     }
 
     @Test
-    void testPaperBeatsRock() {
-        Move paper = new Paper();
-        Move rock = new Rock();
-        assertTrue(paper.beats(rock), "Paper should beat Rock");
+    void testMainPaperVsRock() {
+        String simulatedInput = "2\n";
+        InputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        Random random = new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 0;
+            }
+        };
+        SimpleRPS.main(new String[0]);
+        String output = outputStream.toString();
+        assertTrue(output.contains("You: Paper"));
+        assertTrue(output.contains("Machine: Rock"));
+        assertTrue(output.contains("You win!"));
+        System.setIn(System.in);
+        System.setOut(System.out);
     }
 
     @Test
-    void testScissorsBeatsPaper() {
-        Move scissors = new Scissors();
-        Move paper = new Paper();
-        assertTrue(scissors.beats(paper), "Scissors should beat Paper");
-    }
-
-    @Test
-    void testTie() {
-        Move rock = new Rock();
-        Move anotherRock = new Rock();
-        assertFalse(rock.beats(anotherRock), "Rock should tie with Rock");
-    }
-
-    @Test
-void testPlayerInputRock() {
-    Scanner scanner = mock(Scanner.class);
-    when(scanner.nextInt()).thenReturn(1);
-
-    Random random = mock(Random.class);
-    when(random.nextInt(3)).thenReturn(0);
-
-    String[] moves = {new Rock().getName(), new Paper().getName(), new Scissors().getName()};
-    String playerMove = moves[0];
-    String machineMove = moves[0];
-
-    assertEquals("Rock", playerMove);
-    assertEquals("Rock", machineMove);
-}
-
-
-@Test
-void testPlayerInputPaperVsRock() {
-    Scanner scanner = mock(Scanner.class);
-    when(scanner.nextInt()).thenReturn(2);
-
-    Random random = mock(Random.class);
-    when(random.nextInt(3)).thenReturn(0);
-
-    String[] moves = {new Rock().getName(), new Paper().getName(), new Scissors().getName()};
-    String playerMove = moves[1];
-    String machineMove = moves[0];
-
-    assertEquals("Paper", playerMove);
-    assertEquals("Rock", machineMove);
-}
-
-
-    @Test
-    void testPlayerInputScissorsVsPaper() {
-        Scanner scanner = mock(Scanner.class);
-        when(scanner.nextInt()).thenReturn(3);
-
-        Random random = mock(Random.class);
-        when(random.nextInt(3)).thenReturn(2);
-
-        String[] moves = {new Rock().getName(), new Paper().getName(), new Scissors().getName()};
-        String playerMove = moves[2];
-        String machineMove = moves[2];
-
-        assertEquals(playerMove, "Scissors");
-        assertEquals(machineMove, "Scissors");
-    }
-
-    @Test
-    void testMachineWinsWithRandomChoice() {
-        Scanner scanner = mock(Scanner.class);
-        when(scanner.nextInt()).thenReturn(1);
-
-        Random random = mock(Random.class);
-        when(random.nextInt(3)).thenReturn(2);
-
-        String[] moves = {new Rock().getName(), new Paper().getName(), new Scissors().getName()};
-        String playerMove = moves[0];
-        String machineMove = moves[2];
-
-        assertEquals(playerMove, "Rock");
-        assertEquals(machineMove, "Scissors");
+    void testMainScissorsVsPaper() {
+        String simulatedInput = "3\n";
+        InputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        Random random = new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 1;
+            }
+        };
+        SimpleRPS.main(new String[0]);
+        String output = outputStream.toString();
+        assertTrue(output.contains("You: Scissors"));
+        assertTrue(output.contains("Machine: Paper"));
+        assertTrue(output.contains("You win!"));
+        System.setIn(System.in);
+        System.setOut(System.out);
     }
 }
